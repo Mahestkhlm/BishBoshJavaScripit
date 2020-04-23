43270c7f7cadac177444da09883c2bdb65472533
Mahe <mahe22mca@gmail.com>
2020-04-23 22:06:00 +0200

    Add project files.

---
+++ BishBosh2.0/script/BishBosh.js
@@ -0,0 +1,16 @@
+﻿function myFunction(p1, bishnumber, boshnumber) {
+    var text = "";
+    for (i = 1; i <= p1; i++) {
+        if (i % bishnumber == 0 && i % boshnumber == 0) {
+            text += "Bish-Bosh" + ", ";
+        } else if (i % bishnumber == 0) {
+            text += "Bish" + ", ";
+        } else if (i % boshnumber == 0) {
+            text += "Bosh" + ", ";
+        } else {
+            text += i + ", ";
+        }
+    }
+    return text;
+}
+//very simple code
